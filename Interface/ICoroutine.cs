72cf1cadc23448e36179574f831a0fc2c1ef10a9
Christian Schladetsch <christian.schladetsch@gmail.com>
2012-08-28 01:04:57 +1100

    -DOC: ICoroutine

--- Interface/ICoroutine.cs
+++ Interface/ICoroutine.cs
@@ -3,7 +3,7 @@
 namespace Flow
 {
 	/// <summary>
-	/// A coroutine is a software implementation of a microthread that can resume after a yield.
+	/// A coroutine is a continuation that can resume after a yield.
 	/// </summary>
 	public interface ICoroutine<TR> : ITypedGenerator<TR>
 	{
