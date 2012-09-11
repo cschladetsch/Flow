990b681796d089a2ef0e9becf0d30daa3da97a5c
Christian Schladetsch <christian.schladetsch@gmail.com>
2012-09-11 13:37:23 +1000

    -CHANGE: targetting .Net 2.0

--- Create.cs
+++ Create.cs
@@ -1,5 +1,7 @@
 // (C) 2012 Christian Schladetsch. See http://www.schladetsch.net/flow/license.txt for Licensing information.
 
+using System;
+
 namespace Flow
 {
 	/// <summary>
@@ -37,4 +39,11 @@ namespace Flow
 			return factory;
 		}
 	}
+
+
+}
+
+namespace System.Runtime.CompilerServices
+{
+    public class ExtensionAttribute : Attribute { }
 }
