e1cfee697e8e883f40781d9bd1448d11b34cff66
Christian <christian@twobulls.com>
2017-11-24 17:32:26 +1100

    Make VS2017 lib and test projects

---
+++ TestFlow/TestFlow/TestActionSequence.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+namespace Flow.Test
+{
+	public class TestActionSequence : TestBase
+	{
+		[Test()]
+		public void TestSequence1()
+		{
+		}
+
+		[Test()]
+		public void TestParallel1()
+		{
+			var total = 0;
+			//var node = _factory.Parallel(
+			//	() => total++,
+			//	() => total++,
+			//	() => total++,
+			//	() => total++
+   //       	);
+
+			//kernel.Root.Add(node);
+			//kernel.Step();
+			//kernel.Step();
+
+			//Assert.AreEqual (4, total);
+		}
+	}
+}
---
+++ TestFlow/TestFlow/TestLoops.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using NUnit.Framework;
+
+namespace Flow.Test
+{
+	public class TestLoops : TestBase
+	{
+		[Test]
+		public void TestDebugLog()
+		{
+			_root.Add(_factory.DebugLog("Hello World"));
+			Step(5);
+		}
+
+		[Test]
+		public void TestLoop()
+		{
+			var f = _factory;
+			_root.Add(
+				f.Do(() => count = 0),
+				f.Loop(
+					f.Coroutine(CountTo, 10)
+				)
+			);
+
+			count = 0;
+			Step(20);
+			Assert.AreEqual(count, 10);
+		}
+
+		private int count = 0;
+
+		IEnumerator CountTo(IGenerator self, int max)
+		{
+			while (++count != max)
+			{
+				yield return 0;
+			}
+		}
+
+		[Test]
+		public void TestConditionalLoop()
+		{
+			var f = _factory;
+			var count = 0;
+			_root.Add(f.While(() => ++count < 5, f.DebugLog("count={0}", count)));
+			Step(10);
+			Print(count);
+			Assert.AreEqual(count, 5);
+		}
+	}
+}
