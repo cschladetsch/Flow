bdb29384e3345d5d61173287138a5170a52b14d9
Christian Schladetsch <christian.schladetsch@gmail.com>
2018-05-27 22:14:02 +1000

    tabs to spaces

--- Impl/TimeFrame.cs
+++ Impl/TimeFrame.cs
@@ -4,13 +4,13 @@ using System;
 
 namespace Flow.Impl
 {
-	/// <summary>
-	/// TODO: delta-capping, pausing, introduction of zulu/sim time differences
-	/// </summary>
-	internal class TimeFrame : ITimeFrame
-	{
-		public DateTime Last { get; internal set; }
-		public DateTime Now { get; internal set; }
-		public TimeSpan Delta { get; internal set; }
-	}
+    /// <summary>
+    /// TODO: delta-capping, pausing, introduction of zulu/sim time differences
+    /// </summary>
+    internal class TimeFrame : ITimeFrame
+    {
+        public DateTime Last { get; internal set; }
+        public DateTime Now { get; internal set; }
+        public TimeSpan Delta { get; internal set; }
+    }
 }
