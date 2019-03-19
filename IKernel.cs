bc503edad61bd14ba32c300dce6f356287960c1b
Christian Schladetsch <christian.schladetsch@gmail.com>
2019-03-19 20:51:19 +1100

    Update

--- IKernel.cs
+++ IKernel.cs
@@ -1,27 +1,19 @@
-// (C) 2012-2018 Christian Schladetsch. See https://github.com/cschladetsch/Flow.
+// (C) 2012-2019 Christian Schladetsch. See https://github.com/cschladetsch/Flow.
 
 using System;
 
 namespace Flow
 {
-    public enum EDebugLevel
-    {
-        None,
-        Low,
-        Medium,
-        High,
-        Verbose,
-    }
-
     /// <inheritdoc />
     /// <summary>
     /// A Kernel contains a top-entryType root Node, and a local TimeFrame.
     /// <para>
-    /// When the Kernel is Stepped, it updates its Time property, Steps the top-entryType Root node, then calls Post on the
-    /// top-entryType Root node.
+    /// When the Kernel is Stepped, it updates its Time property, Steps the Root node, then calls Post on the
+    /// Root node.
     /// </para>
     /// </summary>
-    public interface IKernel : IGenerator
+    public interface IKernel 
+        : IGenerator
     {
         EDebugLevel DebugLevel { get; set; }
         ILogger Log { get; set; }
@@ -30,11 +22,9 @@ namespace Flow
         INode Root { get; set; }
         IFactory Factory { get; }
         ITimeFrame Time { get; }
+
         void Update(float deltaSeconds);
-        void Wait(TimeSpan end);
+        void Wait(TimeSpan duration);
         void BreakFlow();
-        //void WaitSteps(int numSteps);
-        //void StepTime();
-        //void ContinueFlow();
     }
 }
