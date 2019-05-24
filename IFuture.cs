a8f7a948bdc17d8e2bae5e70b90bec6bc9ece246
Christian <christian@liminalvr.com>
2019-05-24 16:23:05 +1000

    Add note for IFuture

--- IFuture.cs
+++ IFuture.cs
@@ -1,5 +1,7 @@
 // (C) 2012-2019 Christian Schladetsch. See https://github.com/cschladetsch/Flow.
 
+using System;
+
 namespace Flow
 {
     public delegate void FutureHandler<T>(IFuture<T> future);
@@ -9,7 +11,9 @@ namespace Flow
     /// When its Value property is first set, a Future fires its Arrived event, sets its Available property to true, and
     /// Completes itself.
     /// </summary>
-    public interface IFuture<T> : ITransient
+    [Obsolete("Are you sure you don't want use a ITimedFuture?")]
+    public interface IFuture<T>
+        : ITransient
     {
         bool Available { get; }
         T Value { get; set; }
